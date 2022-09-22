8376fd41522cbc16612ac4e5c3d32de4c03a6996
ONEASH <oneash0824@naver.com>
2022-09-22 22:44:50 +0900

    JPA

--- src/main/java/hello/hellospring2/SpringConfig.java
+++ src/main/java/hello/hellospring2/SpringConfig.java
@@ -1,35 +1,32 @@
 package hello.hellospring2;
 
-import hello.hellospring2.repository.JdbcMemberRepository;
 import hello.hellospring2.repository.MemberRepository;
-import hello.hellospring2.repository.MemoryMemberRepository;
 import hello.hellospring2.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService () {
 
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcMemberRepository(dataSource);
-    }
+//        return new JdbcMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
 }
